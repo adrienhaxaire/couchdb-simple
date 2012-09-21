d75093b483c531ae2f3577552a997eacf8e4da50
adrienhaxaire <adrien@haxaire.org>
2012-09-22 00:13:57 +0200

    adapted last URI change to existing functions

--- Database/SimpleCouch.hs
+++ Database/SimpleCouch.hs
@@ -1,15 +1,8 @@
 module Database.SimpleCouch where
 
 import Network.HTTP hiding (host)
-import Network.Stream
 import Network.URI
-import Text.ParserCombinators.Parsec 
-import qualified Data.List as L
-import Data.Maybe
 import Control.Applicative
-
-type Host = URI
-type DB = URI
 
 type DocId = String
 type DocBody = String
@@ -20,81 +13,85 @@ moduleName = "Database.SimpleCouch"
 contentType :: String
 contentType = "application/json"
 
-db :: String -> Maybe DB
-db urlString = let removeQF x = x {uriQuery = "", uriFragment = ""}
-                   between = takeWhile (/= '/') . drop 1
-                   trimPath y = y {uriPath = '/' : (between $ uriPath y)}
-               in trimPath <$> removeQF <$> parseAbsoluteURI urlString
+-- local helpers
+rmQuery :: URI -> URI
+rmQuery u = u {uriQuery = ""}
 
-host :: String -> Maybe Host 
-host s = (\u -> u {uriPath = ""}) <$> db s
+rmFragment :: URI -> URI
+rmFragment u = u {uriFragment = ""}
 
+rmQF :: URI -> URI
+rmQF = rmFragment . rmQuery
 
-url = "http://localhost:5984/test/fhad8y7"
+rmPath :: URI -> URI
+rmPath u = u {uriPath = ""}
 
-
-{-
-
-
-putRequest :: String -> Request String
-putRequest urlString = 
-    case parseURI urlString of
-      Nothing -> error ("putRequest: Not a valid URL - " ++ urlString)
-      Just u  -> mkRequest PUT u
-
-putRequestWithBody :: String -> String -> Request String
-putRequestWithBody urlString body = 
-  case parseURI urlString of
-    Nothing -> error ("putRequestWithBody: Not a valid URL - " ++ urlString)
-    Just u  -> setRequestBody (mkRequest PUT u) (contentType, body)
-
-deleteRequest :: String -> Request String
-deleteRequest urlString = 
-    case parseURI urlString of
-      Nothing -> error ("deleteRequest: Not a valid URL - " ++ urlString)
-      Just u  -> mkRequest DELETE u
+appendToPath :: URI -> String -> URI
+appendToPath u s = u {uriPath = uriPath u ++ s}
 
 req :: Request String -> IO String
-req r = simpleHTTP r >>= getResponseBody >>= return
+req r = simpleHTTP r >>= getResponseBody
+
+reqWithBody :: URI -> RequestMethod -> DocBody -> IO String
+reqWithBody u m b = req $ setRequestBody (mkRequest m u) (contentType, b)
+
+reqWithPath :: URI -> RequestMethod -> String -> IO String
+reqWithPath u m s = req $ mkRequest m $ appendToPath u s
+
+-- 
+type DB = URI
+
+db :: String -> Maybe DB
+db urlString = let between = takeWhile (/= '/') . drop 1
+                   trimPath y = y {uriPath = '/' : between (uriPath y)}
+               in trimPath <$> rmQF <$> parseAbsoluteURI urlString
+
+type Host = URI
+
+host :: String -> Maybe Host 
+host s = rmPath <$> db s
 
 uuid :: Host -> IO String
 uuid u = do 
-  r <- req $ getRequest (u ++ "/_uuids")
+  r <- reqWithPath u GET "/_uuids"
   return $ take 32 $ drop 11 r
 
 -- ask for a list of uuids
 --uuids :: Host -> Int -> IO [String]
---uuids u = req $ getRequest (host u ++ "/_uuids")
 
 putDB :: DB -> IO String
-putDB = req . putRequest
+putDB = req . mkRequest PUT
 
 deleteDB :: DB -> IO String
-deleteDB = req . deleteRequest
+deleteDB = req . mkRequest DELETE
 
 getDBInfo :: DB -> IO String
-getDBInfo = req . getRequest
+getDBInfo = req . mkRequest GET
 
 getDBChanges :: DB -> IO String
-getDBChanges d = req $ getRequest (d ++ "/_changes") 
-
-putDocWithId :: DB -> DocId -> DocBody -> IO String 
-putDocWithId d docId body = 
-    case db d of
-      Nothing -> error $ moduleName ++ ".putDoc: invalid database: " ++ d
-      Just v -> do 
-                req $ putRequestWithBody (v ++ "/" ++ docId) body
-
-putDoc :: DB -> DocBody -> IO String 
-putDoc d body = do u <- uuid (fromMaybe "" $ host d) 
-                   putDocWithId d u body 
-
+getDBChanges d = reqWithPath d GET "/_changes" 
 
 compactDB :: DB -> IO String
-compactDB d = req $ postRequest (d ++ "/_compact")
+compactDB d = reqWithPath d POST "/_compact"
 
+putDocWithId :: DB -> DocId -> DocBody -> IO String 
+putDocWithId d docId = reqWithBody (appendToPath d ('/' : docId)) PUT
+
+putDoc :: DB -> DocBody -> IO String 
+putDoc d body = do
+  u <- uuid $ rmPath d
+  putDocWithId d u body  
+
+
+
+{-
 
 --compactDBViews
 --cleanupDBViews
 
+
+url = "http://localhost:5984/test/fhad8y7"
+
 -}
+
+
